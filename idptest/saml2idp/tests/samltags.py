5f62f979b8554ca9810adb092b5f6b7275d48b7c
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2011-08-12 19:31:30 +0000

    Added Signature XML test.
    git-svn-id: http://django-saml2-idp.googlecode.com/svn/trunk@22 41e50629-eb6e-b841-421f-ad03d4eac035

--- idptest/saml2idp/tests/samltags.py
+++ idptest/saml2idp/tests/samltags.py
@@ -25,30 +25,29 @@ assertion = {
     },
     'subject': { 'email': 'student@abrickaday.com' },
 }
+
 issuer = 'http://127.0.0.1/simplesaml/saml2/idp/metadata.php'
 
-class TestAssertionXML(TestCase):
+signature = {
+    'reference_uri': '#_2972e82c07bb5453956cc11fb19cad97ed26ff8bb4',
+    'digest': 'hI+IRHRPC77suriMAt4gVVCcQgc=',
+    'value': 'JJObb8Wc37VgBpD1bxMm6pStovjiK7wbL9U/vI1f3aIjIZrtZ2XfwzZiXoDROS0Aov33lNhM9yypZepQJpRrABZoxhcpg03C0GBhyREBC2HtWvmMaA6GP5oiojPJ1VCF3ArECht1RSMISLak/YxqiP6vgnQfZkuiKcaA8vEpAsI=',
+    'certificate': 'MIICgTCCAeoCCQCbOlrWDdX7FTANBgkqhkiG9w0BAQUFADCBhDELMAkGA1UEBhMCTk8xGDAWBgNVBAgTD0FuZHJlYXMgU29sYmVyZzEMMAoGA1UEBxMDRm9vMRAwDgYDVQQKEwdVTklORVRUMRgwFgYDVQQDEw9mZWlkZS5lcmxhbmcubm8xITAfBgkqhkiG9w0BCQEWEmFuZHJlYXNAdW5pbmV0dC5ubzAeFw0wNzA2MTUxMjAxMzVaFw0wNzA4MTQxMjAxMzVaMIGEMQswCQYDVQQGEwJOTzEYMBYGA1UECBMPQW5kcmVhcyBTb2xiZXJnMQwwCgYDVQQHEwNGb28xEDAOBgNVBAoTB1VOSU5FVFQxGDAWBgNVBAMTD2ZlaWRlLmVybGFuZy5ubzEhMB8GCSqGSIb3DQEJARYSYW5kcmVhc0B1bmluZXR0Lm5vMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDivbhR7P516x/S3BqKxupQe0LONoliupiBOesCO3SHbDrl3+q9IbfnfmE04rNuMcPsIxB161TdDpIesLCn7c8aPHISKOtPlAeTZSnb8QAu7aRjZq3+PbrP5uW3TcfCGPtKTytHOge/OlJbo078dVhXQ14d1EDwXJW1rRXuUt4C8QIDAQABMA0GCSqGSIb3DQEBBQUAA4GBACDVfp86HObqY+e8BUoWQ9+VMQx1ASDohBjwOsg2WykUqRXF+dLfcUH9dWR63CtZIKFDbStNomPnQz7nbK+onygwBspVEbnHuUihZq3ZUdmumQqCw4Uvs/1Uvq3orOo/WJVhTyvLgFVK2QarQ4/67OZfHd7R+POBXhophSMv1ZOo',
+}
+
+class TestXML(TestCase):
     """
-    Tests for the Assertion section only.
+    Base class for XML TestCases.
     """
-    def _test(self, expfile, saml_request, assertion, issuer=None):
+    def _test_base(self, expfile, template, context):
         """
-        Renders the assertion using these parameters and compares it with
+        Renders the template using the context and compares it with
         expected xml from expfile.
         """
-        # Arrange.
-        t = Template(
-            '{% load samltags %}'
-            '{% assertion_xml saml_request assertion issuer %}'
-        )
-        c = Context({
-            'saml_request': saml_request,
-            'assertion': assertion,
-            'issuer': issuer,
-        })
+        # Arrange - done in sub-classes.
 
         # Act.
-        got = t.render(c)
+        got = template.render(context)
 
         # (Post-act?) Save "gotten" XML, for use with external diff tools.
         gotfilename = EXPECTED_XML_FILENAME % (expfile + '.got')
@@ -66,8 +65,44 @@ class TestAssertionXML(TestCase):
         msg = "Did not get expected XML. See %s." % gotfilename
         self.assertEqual(got, exp, msg)
 
+
+class TestAssertionXML(TestXML):
+    """
+    Tests for the Assertion section only.
+    """
+    def _test(self, expfile, saml_request, assertion, issuer=None):
+        # Arrange.
+        t = Template(
+            '{% load samltags %}'
+            '{% assertion_xml saml_request assertion issuer %}'
+        )
+        c = Context({
+            'saml_request': saml_request,
+            'assertion': assertion,
+            'issuer': issuer,
+        })
+        self._test_base(expfile, t, c)
+
     def test_assertion_without_issuer(self):
         self._test('assertion_without_issuer', saml_request, assertion)
 
     def test_assertion_with_issuer(self):
         self._test('assertion_with_issuer', saml_request, assertion, issuer)
+
+class TestSignatureXML(TestXML):
+    """
+    Tests for the Signature section only.
+    """
+    def _test(self, expfile, signature):
+        # Arrange.
+        t = Template(
+            '{% load samltags %}'
+            '{% signature_xml signature %}'
+        )
+        c = Context({
+            'signature': signature,
+        })
+        self._test_base(expfile, t, c)
+
+    def test_signature(self):
+        self._test("signature", signature)
