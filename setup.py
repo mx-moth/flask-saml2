b210da16c394470b6f1ee1b4606183cd61cbd57d
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2011-10-06 21:56:11 +0000

    Updated docs; include tests package in egg.
    git-svn-id: http://django-saml2-idp.googlecode.com/svn/trunk@125 41e50629-eb6e-b841-421f-ad03d4eac035

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from setuptools import setup
 
 setup(
     name = 'saml2idp',
-    version = '0.1',
+    version = '0.12',
     author = 'John Samuel Anderson',
     author_email = 'john@andersoninnovative.com',
     description = 'SAML 2.0 IdP for Django',
     long_description = 'SAML 2.0 Identity Provider app for Django projects.',
     install_requires = ['M2Crypto==0.20.1'],
     license = 'MIT',
-    packages = ['saml2idp'],
+    packages = ['saml2idp', 'saml2idp.tests'],
     package_dir = {'saml2idp': 'idptest/saml2idp'},
+    package_data = {'saml2idp': ['templates/saml2idp/*.html']},
     url = 'http://code.google.com/p/django-saml2-idp/',
     zip_safe = True,
 )
