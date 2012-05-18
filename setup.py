2af7c6e5c5717d72996b1b79f3b9356d74ca327a
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2012-05-18 15:31:08 +0000

    Allow M2Crypto>=0.20.1 in setup.py; fixes Issue #8
    git-svn-id: http://django-saml2-idp.googlecode.com/svn/trunk@141 41e50629-eb6e-b841-421f-ad03d4eac035

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email = 'john@andersoninnovative.com',
     description = 'SAML 2.0 IdP for Django',
     long_description = 'SAML 2.0 Identity Provider app for Django projects.',
-    install_requires = ['M2Crypto==0.20.1'],
+    install_requires = ['M2Crypto>=0.20.1'],
     license = 'MIT',
     packages = ['saml2idp', 'saml2idp.tests'],
     package_dir = {'saml2idp': 'idptest/saml2idp'},
