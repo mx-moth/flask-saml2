d7d381d79f98e19389dcf18ef053ee84a42febbc
john@andersoninnovative.com <john@andersoninnovative.com@41e50629-eb6e-b841-421f-ad03d4eac035>
2012-05-18 20:44:06 +0000

    Fleshed out IDPSSODescriptor view.
    git-svn-id: http://django-saml2-idp.googlecode.com/svn/trunk@145 41e50629-eb6e-b841-421f-ad03d4eac035

--- idptest/saml2idp/views.py
+++ idptest/saml2idp/views.py
@@ -6,12 +6,17 @@ import uuid
 # Django/other library imports:
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response, redirect
 from django.views.decorators.csrf import csrf_view_exempt, csrf_response_exempt
 # saml2idp app imports:
 import saml2idp_settings
 import exceptions
 import registry
+import xml_signing
+
+def xml_response(request, template, tv):
+    return render_to_response(template, tv, mimetype="application/xml")
 
 @csrf_view_exempt
 def login_begin(request, *args, **kwargs):
@@ -62,10 +67,10 @@ def descriptor(request):
     """
     Replies with the XML Metadata IDSSODescriptor.
     """
-    entity_id = None
-    slo_url = None
-    sso_url = None
-    pubkey = None
+    entity_id = saml2idp_settings.SAML2IDP_ISSUER
+    slo_url = request.build_absolute_uri(reverse('logout'))
+    sso_url = request.build_absolute_uri(reverse('login_begin'))
+    pubkey = xml_signing.load_cert_data(saml2idp_settings.SAML2IDP_CERTIFICATE_FILE)
     tv = {
         'entity_id': entity_id,
         'cert_public_key': pubkey,
