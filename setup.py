1f0c119f5476fab7bd2d12be3fcba854df35140b
Gabriel de Forest <gabe@zipari.com>
2017-06-26 11:52:30 -0400

    divergence for pypi because i need to release this

--- setup.py
+++ setup.py
@@ -12,22 +12,20 @@ with open('HISTORY.rst') as history:
 
 
 setup(
-    name='dj-saml-idp',
+    name='django-saml-idp',
     version=saml2idp.__version__,
-    author='Sebastian Vetter',
+    author='Gabriel de Forest, Sebastian Vetter',
     author_email='sebastian@mobify.com',
     description='SAML 2.0 IdP for Django',
     long_description='\n\n'.join([description, changelog]),
     install_requires=[
         'Django>=1.4',
-        # We have to pin M2Crypto to version 0.22.3 because more recent
-        # versions are failing due to issues with finding openssl libs.
         'M2Crypto==0.26.0',
         'BeautifulSoup>=3.2.0',
         'structlog'],
     license='MIT',
     packages=['saml2idp'],
-    url='http://github.com/mobify/dj-saml-idp',
+    url='http://github.com/deforestg/dj-saml-idp',
     zip_safe=False,
     include_package_data=True,
 )
