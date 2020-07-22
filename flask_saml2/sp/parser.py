7da4a8a55599746bf95829bf6df694a047259af9
Tanishq Dubey <tanishq.dubey@gmail.com>
2020-07-22 00:10:45 -0400

    pass tox tests

--- flask_saml2/sp/parser.py
+++ flask_saml2/sp/parser.py
@@ -1,4 +1,4 @@
-from typing import Mapping, Optional, List, Union
+from typing import List, Mapping, Optional, Union
 
 from flask_saml2.types import XmlNode
 from flask_saml2.utils import cached_property
